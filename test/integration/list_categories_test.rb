6ee59e648f892eac4e16c6cf308c161123ee1c8b
anil kumar <siva.teki@prasthana.com>
2022-04-06 18:00:58 +0530

    test-integration

---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+     @category= Category.create(name: "sports")
+     @category2= Category.create(name:"travel")
+
+  end
+  test "should show category list " do
+  get '/categories'
+  assert_select "a[href=?]", category_path(@category), text:@category.name
+  assert_select "a[href=?]", category_path(@category2), text:@category2.name
+  end
+  
+end
