21b6a1b0406d621904dd6bbb88ec6a75485c4cb6
anil kumar <siva.teki@prasthana.com>
2022-04-07 18:03:25 +0530

    many-many back end

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+
+    @admin_user=Sign.create(username:"anilpj", mail:"anilpjmail.com",password:"password",
+                            admin: true)
+    sign_in_as(@admin_user)
+  end
 
    test "get  new category form and create category" do
      get "/categories/new"
      assert_response :success
 
      assert_difference'Category.count', 1 do
-       post categories_path, params: { category: {name: "sports"  } }
+       post categories_path, params: { category: {name:"sports"  } }
       assert_response :redirect
    end
        follow_redirect!
